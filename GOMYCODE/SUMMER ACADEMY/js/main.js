c3f7df5b96e762c40709997ed2f4883259f9f172
najjarbechir56 <52280871+najjarbechir56@users.noreply.github.com>
2019-07-04 14:28:16 +0100

    Add files via upload

---
+++ GOMYCODE/SUMMER ACADEMY/js/main.js
@@ -0,0 +1,19 @@
+var Bb= document.getElementById("bechir");
+function B() {
+ Bb.style.fontWeight="bold"   
+}
+function A() {
+    Bb.style.fontStyle="italic"
+}
+function C() {
+    Bb.style.textDecoration="underline"
+}
+var x=document.getElementById("az");
+function ff(){
+    Bb.style.fontSize=x.value
+}
+var h=document.getElementById("zz");
+function fu() {
+    Bb.style.fontFamily=zz.value
+}
+
